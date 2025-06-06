392cdb458e5cd98f2d611757ef318227a96096b7
gvukamolle <tim.grushko7@gmail.com>
2025-06-06 18:43:42 +0300

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.calorietracker"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.calorietracker"
         minSdk = 31
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
